4ff0b38b1c755996e7e8579ab999b6e00ff34787
Semjon Kerner <Semjon.Kerner@gmx.net>
2021-11-21 18:23:31 +0100

    update 1.4.4

--- lang/fr.lua
+++ lang/fr.lua
@@ -1,9 +1,9 @@
 --Big thanks to Frackou for translating this to french!
-SafeAddString(FISHERMANSFRIEND_LAKE_FISHING_HOLE, "Trou de pêche lacustre",	0)
-SafeAddString(FISHERMANSFRIEND_SALT_FISHING_HOLE, "Trou de pêche d'eau de mer", 	0)
-SafeAddString(FISHERMANSFRIEND_MYST_FISHING_HOLE, "Trou de pêche de mystique",0)
+SafeAddString(FISHERMANSFRIEND_LAKE_FISHING_HOLE, "Trou de pêche lacustre", 0)
+SafeAddString(FISHERMANSFRIEND_SALT_FISHING_HOLE, "Trou de pêche d'eau de mer", 0)
+SafeAddString(FISHERMANSFRIEND_MYST_FISHING_HOLE, "Trou de pêche mystique",0)
 SafeAddString(FISHERMANSFRIEND_FOUL_FISHING_HOLE, "Trou de pêche sale", 0)
-SafeAddString(FISHERMANSFRIEND_OILY_FISHING_HOLE, "Trou de pêche de huileux", 	0)
+SafeAddString(FISHERMANSFRIEND_OILY_FISHING_HOLE, "Trou de pêche huileux", 0)
 SafeAddString(FISHERMANSFRIEND_RIVR_FISHING_HOLE, "Trou de pêche de rivière", 0)
 
 SafeAddString(FISHERMANSFRIEND_CNF_DESCRIPTION, "Fisherman's Friend permet d'équiper automatiquement l'appât approprié lorsque vous regardez un trou de pêche et d'en changer lorsque vous manquez d'un type. Ci-dessous, vous pouvez choisir si vous souhaitez ou non utiliser d'abord les appâts alternatifs (Shad, Chevesne, Œufs de poisson et Méné) plutôt que les appâts ordinaires (Morceaux d'insectes, Vers, Rampants, Entrailles) ainsi qu'afficher un message vous indiquant que vous n'avez pas l'appât approprié dans votre sac.", 0)
